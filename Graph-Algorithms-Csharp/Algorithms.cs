eacf212fdfb11ffc2d5d40b0bb57d3df28d69f10
mohammed ebrahim fitooh eissa <eissa4444@yahoo.com>
2017-06-08 01:12:16 +0200

    implementing braedth first search

--- Graph-Algorithms-Csharp/Algorithms.cs
+++ Graph-Algorithms-Csharp/Algorithms.cs
@@ -28,5 +28,31 @@ namespace Graph_Algorithms_Csharp
             return visited;
 
         }
+        public HashSet<T> Bfs<T>(Graph<T> graph, T start)
+        {
+            var visited = new HashSet<T>();
+            if (graph.AdjacencyList.ContainsKey(start))
+            {
+                return visited;
+            }
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                if (visited.Contains(vertex))
+                    continue;
+                visited.Add(vertex);
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        queue.Enqueue(neighbor);
+                    }
+                }
+
+            }
+            return visited;
+        }
     }
 }
